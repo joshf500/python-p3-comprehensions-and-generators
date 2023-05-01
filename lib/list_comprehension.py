b3f348374a01491f619a19c66f333d2aae058e68
Josh Feldman <joshfeldman500@gmail.com>
2023-05-01 16:28:45 -0400

    comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,14 @@
 
 def return_evens(num_list):
     pass
+    evenlist=[]
+    for i in range (len(num_list)):
+        if (num_list[i]%2)==0:
+            evenlist.append(num_list[i])
+    return evenlist
 
 def make_exclamation(sentence_list):
     pass
+    for i in range (len(sentence_list)):
+        sentence_list[i]=sentence_list[i]+"!"
+    return sentence_list
